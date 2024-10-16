1e8698e8421d6ea97f6be32da5eab4086806c913
InHye Kyeong <kih9079@gmail.com>
2024-10-16 22:24:03 +0900

    module.exports 와 exports

---
+++ var.js
@@ -0,0 +1,16 @@
+const odd = "홀수입니다.";
+const even = "짝수입니다.";
+
+
+// 두 가지 이상 exports할 경우 사용
+exports.odd = odd;
+exports.even = even;
+
+/* 위의 코드나 아래의 코드나 둘 중 하나만 사용.
+module.exports = {
+  odd,
+  even,
+};
+*/
+
+// module.exports === exports === {}
